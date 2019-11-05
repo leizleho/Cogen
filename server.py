5c8fb816760f3a27f45fd3d5e5bae92ddc937f57
Leizle <ho.leizle@gmail.com>
2019-11-05 09:36:23 -0800

    added flask debug toolbar

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from app import app
 from app.models import connect_to_db, db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 connect_to_db(app)
+# Use the DebugToolbar
+DebugToolbarExtension(app)
 app.run(debug=True)
