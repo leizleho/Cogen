61aa0b46493e0221170d62926202d937f64215a0
Leizle <ho.leizle@gmail.com>
2019-11-05 09:59:38 -0800

    added homepage route

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.user import user_routes
+from app.main import main_routes
 app = Flask(__name__)
 
 # temporary secret key
 app.secret_key = "ABC"
 
 app.register_blueprint(user_routes.user_bp)
+app.register_blueprint(main_routes.main_bp)
