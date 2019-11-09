64b8f218aaa875da2b544761cec2e0cfe7be11c4
Leizle <ho.leizle@gmail.com>
2019-11-08 16:08:27 -0800

    added routes for gen module

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from app.user import user_routes
 from app.main import main_routes
 from app.project import project_routes
+from app.gen import gen_routes
+
 app = Flask(__name__)
 
 # temporary secret key
@@ -10,3 +12,4 @@ app.secret_key = "ABC"
 app.register_blueprint(main_routes.main_bp)
 app.register_blueprint(user_routes.user_bp)
 app.register_blueprint(project_routes.project_bp)
+app.register_blueprint(gen_routes.gen_bp)
