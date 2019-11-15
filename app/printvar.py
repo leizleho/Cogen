b807ac40d41ffca97932821ceab88b359009e6e0
Leizle <ho.leizle@gmail.com>
2019-11-15 15:34:32 -0800

    refactored code generator functions

--- app/printvar.py
+++ app/printvar.py
@@ -6,6 +6,3 @@ def printvar(var_name, var_val):
 # ----------- how to use ----------
 # from app.printvar import printvar
 # printvar("schema", schema)
-# printvar("add_fields", add_fields)
-# printvar("edit_fields", edit_fields)
-# printvar("view_fields", view_fields)
