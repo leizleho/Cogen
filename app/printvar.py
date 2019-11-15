a259b3adc859b704a627ca21fbfffd65d35f26a9
Leizle <ho.leizle@gmail.com>
2019-11-14 17:49:23 -0800

    --

---
+++ app/printvar.py
@@ -0,0 +1,11 @@
+def printvar(var_name, var_val):
+    print(f"------------{var_name.upper()}------------")
+    print(var_val)
+    print("\n\n\n")
+
+# ----------- how to use ----------
+# from app.printvar import printvar
+# printvar("schema", schema)
+# printvar("add_fields", add_fields)
+# printvar("edit_fields", edit_fields)
+# printvar("view_fields", view_fields)
