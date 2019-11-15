d8b1a1a24d1057ece8fed17116d17e8f56ae3b11
Leizle <ho.leizle@gmail.com>
2019-11-14 16:26:27 -0800

    dictionary of template files for code generator

---
+++ app/gen/source_dict.py
@@ -0,0 +1,18 @@
+source = {
+    "source__1": "gitignore.txt .gitignore",
+    "source__2": "README.md README.md",
+    "source__3": "requirements.txt requirements.txt",
+    "source__4": "server.txt server.py",
+    "source/app__5": "__init__.txt __init__.py",
+    "source/app/static/css___6": "style.css style.css",
+    "source/app/templates___8": "layout.html layout.html",
+    "source/app/templates___9": "meta.html meta.html",
+    "source/app/templates___10": "navbar_default.html navbar_default.html",
+    "source/app/templates___11": "navbar_loggedin.html navbar_loggedin.html",
+    "source/app/templates___12": "scripts.html scripts.html",
+    "source/app/templates___13": "_render_field.html _render_field.html",
+    "source/app/mod_main___18": "__init__.txt __init__.py",
+    "source/app/mod_main___15": "main_routes.txt main_routes.py",
+    "source/app/mod_main/static/css___16": "style.css style.css",
+    "source/app/mod_main/templates___17": "index.html index.html"
+}
