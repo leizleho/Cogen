d03334b0be57edc51eb5fd62e84618fdd040cf9e
Leizle <ho.leizle@gmail.com>
2019-11-15 09:47:47 -0800

    added some bootstrap style

--- app/project/project_forms.py
+++ app/project/project_forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import InputRequired, Length
 class ProjectForm(FlaskForm):
     name = StringField('Project name', validators=[
         InputRequired("Project name is required"), Length(min=3, max=20, message="Project name must have length between 3 to 20")])
-    description = StringField('Project description', validators=[
+    description = TextAreaField('Project description', validators=[
         InputRequired("Project description is required"), Length(min=20, max=200, message="Project Description must have length between 20 to 200")])
     db_uri = StringField('Database Connection String')
 
