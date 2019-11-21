61db5a62d4bd3a204fb55829d627244607c49ac6
Leizle <ho.leizle@gmail.com>
2019-11-20 18:50:18 -0800

    update page templates model

--- app/models.py
+++ app/models.py
@@ -108,20 +108,27 @@ class PageTemplate(db.Model):
     __tablename__ = 'page_templates'
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    table_id = db.Column(
-        db.Integer, db.ForeignKey('tables.id'), index=True)
-    page = db.Column(db.String(30), nullable=False)
-    template = db.Column(db.String(30), nullable=True)
-    kwargs = db.Column(db.String(30), nullable=True)
+    table_id = db.Column(db.Integer, db.ForeignKey('tables.id'), index=True)
+    list_page = db.Column(db.String(30), nullable=True)
+    list_kwargs = db.Column(db.String(120), nullable=True)
+    add_page = db.Column(db.String(30), nullable=True)
+    add_kwargs = db.Column(db.String(120), nullable=True)
+    edit_page = db.Column(db.String(30), nullable=True)
+    edit_kwargs = db.Column(db.String(120), nullable=True)
+    view_page = db.Column(db.String(30), nullable=True)
+    view_kwargs = db.Column(db.String(120), nullable=True)
+    delete_page = db.Column(db.String(30), nullable=True)
+    delete_kwargs = db.Column(db.String(120), nullable=True)
 
     # Relationship to tables
     table = db.relationship('Table',
-                            backref=db.backref('page_templates', order_by=id))
+                            backref=db.backref('page_templates',
+                                               uselist=False, order_by=id))
 
     def __repr__(self):
         """Field info"""
 
-        return f'<PageTemplate template={self.template} table_id={self.table_id}>'
+        return f'<PageTemplate table_id={self.table_id} list={self.list_page}>'
 
 
 # Additional Features if time allows
