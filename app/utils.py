213d24abc7b20cae75d362b67088416086d38b1e
Leizle <ho.leizle@gmail.com>
2019-11-25 13:59:17 -0800

    --

---
+++ app/utils.py
@@ -0,0 +1,16 @@
+import re
+
+
+def snake_case(name):
+    """Function to convert CamelCase to snake_case"""
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def camel_case(name):
+    words = name.split('_')
+    return ''.join((w.capitalize() for w in words))
+
+
+def title_case(name):
+    return name.replace("_", " ").title()
