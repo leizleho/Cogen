8ca4efe742e872de3775b0f5263692f1ae34b9f4
Leizle <ho.leizle@gmail.com>
2019-11-28 11:49:07 -0800

    added flask jwt, api endpoint for user resource

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
-# from flask_restful import Api
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
+from app.mod_api.blacklist import BLACKLIST
+from app.mod_api.resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
 # from app.api.resources.table import Table, Tables
 # from app.api.resources.project import Project, Projects
 
@@ -9,7 +12,7 @@ app = Flask(__name__)
 
 # temporary secret key
 app.secret_key = "ABC"
-# api = Api(app)
+api = Api(app)
 
 # Flask-Login configs
 login_manager = LoginManager()
@@ -22,7 +25,7 @@ from app.mod_user import user_bp
 from app.mod_main import main_bp
 from app.mod_project import project_bp
 from app.mod_gen import gen_bp
-from app.api import api_bp
+from app.mod_api import api_bp
 
 # Register blueprints with the app
 app.register_blueprint(main_bp)
@@ -31,7 +34,21 @@ app.register_blueprint(project_bp)
 app.register_blueprint(gen_bp)
 app.register_blueprint(api_bp)
 
+
+jwt = JWTManager(app)
+
+# This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token["jti"] in BLACKLIST
+
+
 # Endpoints for the API module
+api.add_resource(UserRegister, "/api/register")
+api.add_resource(User, "/api/user/<int:id>")
+api.add_resource(UserLogin, "/api/login")
+api.add_resource(TokenRefresh, "/api/refresh")
+api.add_resource(UserLogout, "/api/logout")
 # api.add_resource(Table, "/api/table/<string:name>")
 # api.add_resource(Tables, "/api/tables")
 # api.add_resource(Project, "/api/project/<string:name>")
