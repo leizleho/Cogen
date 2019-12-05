efab98e60d5790295ee0cd716a3af890e9985df4
Leizle <ho.leizle@gmail.com>
2019-12-05 12:35:46 -0800

    moved app.run under if __name__ is __main___ so that uwsgi can run the app

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from app import app
 from app.db import db, connect_to_db
 from app.ma import ma
-from flask_debugtoolbar import DebugToolbarExtension
 
 connect_to_db(app)
 ma.init_app(app)
 app.debug = True
-# DebugToolbarExtension(app)
-app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5555)
